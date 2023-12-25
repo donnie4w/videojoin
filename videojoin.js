21bce2547655b20958e81d1896dc6ef84d135962
donnie4w <donnie4w@gmail.com>
2023-12-25 13:14:14 +0800

    Update videojoin.js

--- videojoin.js
+++ videojoin.js
@@ -45,7 +45,7 @@ class Media {
     mediavideo = null;
     constraints = null;
     audioBitsPerSecond = 320000;
-    videoBitsPerSecond = 1000000;
+    videoBitsPerSecond = 500000;
     sendStream = null;
     recorderHz = 300;
     constructor(video, constraints) {
@@ -54,19 +54,15 @@ class Media {
     }
     MediaDevices() {
         if (!VJUtil.isEmpty(navigator.mediaDevices) && navigator.mediaDevices.getUserMedia) {
-            console.log("navigator.mediaDevices");
             navigator.mediaDevices.getUserMedia(this.constraints)
                 .then(stream => this.parseStream(stream)).catch(err => { console.error(`mediaDevices failed: ${err}`) });
         } else if (navigator.webkitGetUserMedia) {
-            console.log("webkitGetUserMedia")
             navigator.webkitGetUserMedia(this.constraints)
                 .then(stream => this.parseStream(stream)).catch(err => { console.error(`mediaDevices failed: ${err}`) });
         } else if (navigator.mozGetUserMedia) {
-            console.log("navigator.mozGetUserMedia")
             navigator.mozGetUserMedia(this.constraints)
                 .then(stream => this.parseStream(stream)).catch(err => { console.error(`mediaDevices failed: ${err}`) });
         } else if (navigator.getUserMedia) {
-            console.log("navigator.getUserMedia")
             navigator.getUserMedia(this.constraints)
                 .then(stream => this.parseStream(stream)).catch(err => { console.error(`mediaDevices failed: ${err}`) });
         } else {
@@ -76,12 +72,11 @@ class Media {
 
     parseStream(stream) {
         this.mediavideo.srcObject = stream;
-        let data = stream;
+        let data = null;
         try {
-            data = this.mediavideo.captureStream()
+            data = this.mediavideo.captureStream();
         } catch (err) {
-            console.error(err)
-            data = stream;
+            data = this.mediavideo.mozCaptureStream();
         }
         this.mediaRecorder = new MediaRecorder(data, {
             audioBitsPerSecond: this.audioBitsPerSecond,
@@ -138,8 +133,9 @@ class Media {
 }
 
 class VideoJoin {
-    vppre = this.crc32(this.stringToUint8Array(Date.now().toString()));
+    vppre = this.crc32(this.stringToUint8Array(new Date().getTime().toString()));
     videoType = "audio/mp3;video/mp4";
+    playEvent = "canplaythrough"
     addcount = 1;
     currentnum = 0;
     loadnum = 1;
@@ -176,12 +172,10 @@ class VideoJoin {
     _addvideo(data, isStream) {
         const count = this.addcount++;
         this.datamap.set(count, this.createvideo(count, data, isStream))
-        console.log(count, "<<<add>>>", this.videoOn);
         if (!this.videoOn) {
             if (this.modeRealtime) {
                 this.callvideo(count)
             } else {
-                console.log(this.currentnum, ",", count)
                 if (this.currentnum <= count) {
                     this.callvideo(this.currentnum++);
                 }
@@ -192,7 +186,9 @@ class VideoJoin {
     Clear() {
         this.turnOn = false;
         if (!VJUtil.isEmpty(this.parentNode)) {
-            this.parentNode.innerHTML = "";
+            while (this.parentNode.firstChild) {
+                this.parentNode.removeChild(this.parentNode.firstChild);
+            }
         }
     }
 
@@ -220,20 +216,25 @@ class VideoJoin {
             video.src = data;
         }
         video.playbackRate = 1;
+        video.className = "1"
         video.style.display = "none";
         video.addEventListener("ended", (e) => {
-            console.log("ended>>>>", count)
             video.pause()
             this.callvideo(count + 1);
         });
         video.addEventListener("error", (e) => {
-            console.log("error>>>>", count, " :", e)
-            this.rmvideo(count, true);
-            if (this.currentnum == count) {
-                this.callvideo(count + 1);
+            if (video.className == "1") {
+                video.load();
+                video.className = "0";
+            } else {
+                // console.log("error>>>>", count, " :", e)
+                this.rmvideo(count, true);
+                if (this.currentnum == count) {
+                    this.callvideo(count + 1);
+                }
             }
         });
-        video.addEventListener('canplay', (e) => {
+        video.addEventListener(this.playEvent, (e) => {
             video.name = this.vname(count);
         })
         return video;
@@ -244,6 +245,7 @@ class VideoJoin {
             const bf = document.getElementById(this.vname(count));
             this.datamap.delete(count);
             if (!VJUtil.isEmpty(bf)) {
+                URL.revokeObjectURL(bf.src)
                 this.parentNode.removeChild(bf);
             }
         } else if (count >= this.delnum) {
@@ -251,24 +253,23 @@ class VideoJoin {
                 const u = document.getElementById(this.vname(i));
                 this.datamap.delete(i);
                 if (!VJUtil.isEmpty(u)) {
+                    URL.revokeObjectURL(u.src)
                     this.parentNode.removeChild(u);
                 }
             }
             this.delnum = count;
         }
-        console.log("delete>>>", this.delnum)
     }
 
     loadvideo(num) {
         let len = this.parentNode.children.length;
-        console.log(len, ", ", this.loadnum, ",", this.addcount);
         if (num > 0) {
             let v = this.datamap.get(num);
             if (!VJUtil.isEmpty(v)) {
                 this.parentNode.appendChild(v);
                 this.datamap.delete(num);
             }
-        } else if (len < 5 && this.loadnum <= this.addcount) {
+        } else if (len < 10 && this.loadnum <= this.addcount) {
             let c = 1;
             let id = this.loadnum
             while (id <= this.addcount) {
@@ -276,7 +277,7 @@ class VideoJoin {
                 if (!VJUtil.isEmpty(v)) {
                     this.parentNode.appendChild(v);
                     this.datamap.delete(id);
-                    if (c++ > 5) {
+                    if (c++ > 10) {
                         break
                     }
                 }
@@ -287,14 +288,14 @@ class VideoJoin {
     }
 
     callvideo(count) {
-        console.log("callvideo>>>", count)
-        this.loadvideo(count);
+        if (!this.turnOn) {
+            return
+        }
+        this.loadvideo();
         if (count > this.currentnum) {
             this.currentnum = count;
         }
-        this.videoOn = true;
         const v = document.getElementById(this.vname(count));
-        console.log(count, "<<<call>>>", !VJUtil.isEmpty(v));
         if (!VJUtil.isEmpty(v)) {
             if (this.modeSpeedRateup) {
                 if (this.datamap.size > 16) {
@@ -306,21 +307,22 @@ class VideoJoin {
                     console.log("playbackRate>>>", v.playbackRate)
                 }
             }
-            this.mvplay(v, count);
+            this.mvplay(v, count, 1);
+        } else if (!VJUtil.isEmpty(this.datamap.get(count))) {
+            this.loadvideo(count);
+            return this.callvideo(count);
+        } else if (this.currentnum < this.loadnum) {
+            for (let i = this.currentnum + 1; i <= this.loadnum; i++) {
+                if (!isEmpty(document.getElementById(this.vname(i)))) {
+                    return callvideo(i)
+                }
+            }
         } else {
-            console.log(count, " not found");
             this.videoOn = false;
-            // if (isEmpty(this.datamap.get(count))) {
-            //   console.log(count, " not found");
-            //   this.videoOn = false;
-            // } else {
-            //   this.loadvideo(count);
-            //   this.callvideo(count);
-            // }
         }
     }
 
-    mvplay(mv, count) {
+    mvplay(mv, count, playcount) {
         const play = mv.play();
         if (play) {
             play.then(() => {
@@ -334,9 +336,14 @@ class VideoJoin {
                 mv.style.display = "inline";
                 this.rmvideo(count - 1, false);
             }).catch(error => {
-                console.log("play failed >>", count, error)
-                this.rmvideo(count, true);
-                this.videoOn = false;
+                if (playcount > 0) {
+                    mv.load();
+                    this.mvplay(mv, count, 0)
+                } else {
+                    console.log("play failed >>", count, error)
+                    this.rmvideo(count, true);
+                    this.videoOn = false;
+                }
             });
         } else {
             console.log("play not exist >>", count)
@@ -370,7 +377,106 @@ class VideoJoin {
         const encoder = new TextEncoder("ISO-8859-1");
         return encoder.encode(text);
     }
+}
 
+
+
+
+class VideoPlayer {
+    mediaRecorder = null;
+    pushon = true;
+    mediavideo = null;
+    sendStream = null;
+    recorderHz = 300;
+    audioBitsPerSecond = 320000;
+    videoBitsPerSecond = 500000;
+    sampleRate = 44100;
+    videofile = null;
+    EndEventFunc = null;
+    constructor(video, videofile) {
+        this.mediavideo = video;
+        this.videofile = videofile;
+        this.mediavideo.addEventListener("ended", (e) => {
+            this.mediavideo.pause()
+            this.pushon = false;
+            if (!VJUtil.isEmpty(this.EndEventFunc)) {
+                this.EndEventFunc()
+            }
+        });
+    }
+
+    Play() {
+        if (!window.FileReader) {
+            throw new Error("the browser does not support this feature");
+        }
+        let reader = new FileReader();
+        reader.readAsArrayBuffer(this.videofile);
+        reader.onload = () => {
+            this.mediavideo.src = URL.createObjectURL(new Blob([new Uint8Array(reader.result)]));
+            let data = null;
+            try {
+                data = this.mediavideo.captureStream();
+            } catch (err) {
+                data = this.mediavideo.mozCaptureStream();
+            }
+            this.mediaRecorder = new MediaRecorder(data, {
+                audioBitsPerSecond: this.audioBitsPerSecond,
+                videoBitsPerSecond: this.videoBitsPerSecond,
+
+            });
+            this.mediaRecorder.ondataavailable = event => {
+                if (!VJUtil.isEmpty(this.sendStream)) {
+                    event.data.arrayBuffer().then(buffer => {
+                        this.sendStream(buffer)
+                    })
+                }
+            };
+            this.mediaRecorder.onstop = () => {
+            };
+        };
+        reader.onerror = () => {
+            console.error(reader.error);
+        }
+    }
+
+    MediaRecorderStop() {
+        this.pushon = false;
+        try {
+            if (this.mediaRecorder.state == 'active') {
+                this.mediaRecorder.stop();
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    Pushstream(on) {
+        if (!this.pushon) {
+            return
+        }
+        if (on) {
+            if (VJUtil.isEmpty(this.mediaRecorder)) {
+                VJUtil.sleep(this.recorderHz).then(() => this.Pushstream(on))
+                return
+            }
+            try {
+                if (this.mediaRecorder.state == 'active') {
+                    this.mediaRecorder.stop();
+                }
+                this.mediaRecorder.start();
+            } catch (err) {
+                console.error(err);
+            }
+            VJUtil.sleep(this.recorderHz).then(() => this.Pushstream(!on));
+        } else {
+            try {
+                this.mediaRecorder.stop();
+            } catch (err) {
+                console.error(err);
+            }
+            this.Pushstream(!on);
+        }
+    }
 }
 
 
